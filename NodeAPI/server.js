097abaab18aae4d26209b4e791e2f63d3332f1d8
kocha <balanoleksyy@gmail.com>
2020-09-21 15:51:37 +0300

    first

---
+++ NodeAPI/server.js
@@ -0,0 +1,50 @@
+var express = require('express'); 
+var app = express(); 
+var log = require('./libs/log')(module);
+
+
+app.get('/api', function(req, res) { 
+    res.send('API is running'); 
+}); 
+ 
+app.listen(1337, function() { 
+    console.log('Express server listening on port 1337');
+}); 
+
+
+app.get('/api/articles', function(req, res) { 
+    res.send('This is not implemented now'); 
+    }); 
+    app.post('/api/articles', function(req, res) { 
+    res.send('This is not implemented now'); 
+    }); 
+    app.get('/api/articles/:id', function(req, res) { 
+    res.send('This is not implemented now'); 
+    }); 
+    app.put('/api/articles/:id', function(req, res) { 
+    res.send('This is not implemented now'); 
+    }); 
+    app.delete('/api/articles/:id', function(req, res) { 
+    res.send('This is not implemented now'); 
+    });
+
+app.get('/ErrorExample', function(req, res, next) { 
+    next(new Error('Random error!')); 
+}); 
+
+app.use(function(req, res, next) { 
+    res.status(404); 
+    log.debug('Not found URL: '+ req.url); 
+    res.send({ error: 'Not found'}); 
+    return next(); 
+}); 
+
+app.use(function(err, req, res, next) { 
+    res.status(err.status|| 500); 
+    log.error('Internal error('+ res.statusCode+ '): '+ err.message);
+    res.send({ error: err.message}); 
+    return next(); 
+});
+
+   
+
